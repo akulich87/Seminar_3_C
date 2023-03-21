2b1204d297c0b326f9b48767e0396f184b17bd93
akulich87 <amgrupp_project@mail.ru>
2023-03-21 03:00:05 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапозон возможных координат точек в этой четверти (x и y)
+
+Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch(number) // передаем переменную ту, по которой мы сравниваем
+{
+  case 1: // если 1
+  {
+    Console.WriteLine(" X > 0 и Y > 0 ");
+    break;
+  }
+  case 2:
+  {
+    Console.WriteLine(" X < 0 и Y > 0 ");
+    break;
+  }
+  case 3:
+  {
+    Console.WriteLine(" X < 0 и Y < 0 ");
+    break;
+  }
+  case 4:
+  {
+    Console.WriteLine(" X > 0 и Y > 0 ");
+    break;
+  }
+
+  default:
+  {
+    Console.WriteLine("Такой четверти нет ");
+    break;
+  }
+}
